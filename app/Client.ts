9105f404c30fcd3073e68c7cc712e50071143d7a
meluiz <me.luizfelipe@gmail.com>
2023-08-19 03:29:34 -0300

    feat(Client.ts): switch from discord.js to discordx for better command handling feat(Client.ts): add logger to display bot status on login refactor(Client.ts): modify constructor to accept simpleCommand option for better command configuration feat(Client.ts): override login method to include logging and bot readiness check

--- app/Client.ts
+++ app/Client.ts
@@ -1,17 +1,27 @@
 import type { ClientOptions } from 'App/Types'
 
-import { Client as DicordClient } from 'discord.js'
+import { logger } from '@poppinss/cliui'
+import { Client as DicordXClient } from 'discordx'
 
 /**
  * Custom client class that extends the Discord.js Client class.
  * This class represents a Discord bot client.
  */
-export class Client extends DicordClient {
+export class Client extends DicordXClient {
   /**
    * Constructs a new instance of the Client class.
    * @param options - The options to configure the client.
    */
   constructor(options: ClientOptions) {
+    options.simpleCommand = options.commands
     super(options)
+  }
+
+  async login(token: string) {
+    if (!this.silent) {
+      logger.info(`${this.user?.username ?? this.botId} is ready!`) // Log a message indicating that the bot is starting
+    }
+
+    return super.login(token)
   }
 }
