5ce7d1dd674a7a65b2f2ba20dab9587c76197b19
meluiz <me.luizfelipe@gmail.com>
2023-08-20 22:25:18 -0300

    refactor(Env/index.ts): change env variable case from lowercase env to uppercase Env for better semantics feat(Env/index.ts): add SPOTIFY_CLIENT_ID and SPOTIFY_CLIENT_SECRET to environment variables to support Spotify integration

--- utils/Env/index.ts
+++ utils/Env/index.ts
@@ -1,15 +1,19 @@
 import { createEnv } from '@t3-oss/env-nextjs'
 import { z } from 'zod'
 
-const env = createEnv({
+const Env = createEnv({
   server: {
     DISCORD_ID: z.string(),
     DISCORD_TOKEN: z.string(),
+    SPOTIFY_CLIENT_ID: z.string(),
+    SPOTIFY_CLIENT_SECRET: z.string(),
   },
   runtimeEnv: {
     DISCORD_ID: process.env.DISCORD_ID,
     DISCORD_TOKEN: process.env.DISCORD_TOKEN,
+    SPOTIFY_CLIENT_ID: process.env.SPOTIFY_CLIENT_ID,
+    SPOTIFY_CLIENT_SECRET: process.env.SPOTIFY_CLIENT_SECRET,
   },
 })
 
-export default env
+export default Env
